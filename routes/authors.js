c128dfdfe98e251419d608e96ea3b033a56051ac
Sergey Lomov <lomserg@gmail.com>
2022-07-11 17:51:00 +0300

    initial author routes

---
+++ routes/authors.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { find } = require('../models/author');
+const router = express.Router();
+const Author = require('../models/author')
+
+// All authors route
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+      }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query 
+        })
+    } catch {
+        res.redirect('/')
+    }
+  
+})
+
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author()})
+})
+// create Author routr
+router.post('/', async (req, res)=> {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        //res.redirect('authors/${newAuthor}')
+        res.redirect('authors')
+    } catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error'
+    })
+} 
+})
+
+module.exports = router
